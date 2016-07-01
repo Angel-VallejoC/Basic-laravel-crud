3bd79cfda2debfdbf755a7b86c4602ecbeccdcab
Angel Vallejo <a.vallejoch@gmail.com>
2016-06-30 23:00:46 -0500

    Create the index view

---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,37 @@
+@extends('../master')
+
+@section('title', 'Students list')
+
+@section('content')
+	<h2>Lista de Alumnos</h2>
+	<hr>
+	<div class="students">
+		@if(count($students) > 0)
+			<table class="table">
+				<thead>
+					<th>Name</th>
+					<th>Last name</th>
+					<th>Grade</th>
+					<th>Actions</th>
+				</thead>
+				<tbody>
+				@foreach($students as $student)				
+					<tr>
+						<td>{{$student->nombre}}</td>
+						<td>{{$student->apellido}}</td>
+						<td>{{$student->grupo}}</td>												
+						<td>
+							<a href="{{ route('students.edit', [$student->id]) }}" class="btn btn-primary"><span class="glyphicon glyphicon-edit"></span></a>
+							<a href="{{ route('students.delete', [$student->id]) }}" class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></a>			
+						</td>
+					</tr>
+				@endforeach
+				</tbody>
+			</table>								
+		@else
+			<h3>There is no students</h3>
+		@endif
+		
+		<a href=" {{ route('students.add') }} " class="btn btn-success pull-right"><span class="glyphicon glyphicon-plus"></span>Add student</a>
+	</div>
+@endsection
